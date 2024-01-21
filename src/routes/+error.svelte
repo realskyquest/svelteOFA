168cca61cc05a4889270a849e67ada0449a9a2a8
realskyquest <135049479+realskyquest@users.noreply.github.com>
2024-01-22 02:02:36 +0500

    Added error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<article class="prose mt-4 ml-4">
+	<h1>{$page.status}</h1>
+	<p>{$page.error?.message}</p>
+</article>
+
+<div class="h-[56vh]" />
