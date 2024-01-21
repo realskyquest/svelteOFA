3277ae4d55d7ebeb3fab693e4179ae758d66d20d
realskyquest <135049479+realskyquest@users.noreply.github.com>
2024-01-22 02:01:11 +0500

    Updated links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,9 +4,9 @@
 	import { onMount } from 'svelte';
 	import { dev } from '$app/environment';
 
-	import { websiteConfig, websitePWAConfig } from '$lib/config/website';
-	import Header from '$lib/config/Header.svelte';
-	import Footer from '$lib/config/Footer.svelte';
+	import { websiteConfig, websitePWAConfig } from '$config/website';
+	import Header from '$config/Header.svelte';
+	import Footer from '$config/Footer.svelte';
 
 	async function detectSWUpdate() {
 		const registration = await navigator.serviceWorker.ready;
@@ -36,11 +36,19 @@
 	<meta name="author" content={websiteConfig.author} />
 	<meta name="generator" content="Sveltekit" />
 
-	<link rel="manifest" href="/manifest.webmanifest" type="application/manifest+json" />
-	<link rel="icon" href="/favicon.ico" sizes="48x48" />
-	<link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
-	<link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
-	<link rel="mask-icon" href="/pwa-512x512.png" color={websitePWAConfig.background_color} />
+	<link
+		rel="manifest"
+		href="{websiteConfig.url}/manifest.webmanifest"
+		type="application/manifest+json"
+	/>
+	<link rel="icon" href="{websiteConfig.url}/favicon.ico" sizes="48x48" />
+	<link rel="icon" href="{websiteConfig.url}/favicon.svg" sizes="any" type="image/svg+xml" />
+	<link rel="apple-touch-icon" href="{websiteConfig.url}/apple-touch-icon-180x180.png" />
+	<link
+		rel="mask-icon"
+		href="{websiteConfig.url}/pwa-512x512.png"
+		color={websitePWAConfig.background_color}
+	/>
 	<meta name="theme-color" content={websitePWAConfig.background_color} />
 </svelte:head>
 
