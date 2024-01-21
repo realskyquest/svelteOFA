755d25f0b1400071ed593ec2db18aaf9de64df24
realskyquest <135049479+realskyquest@users.noreply.github.com>
2024-01-22 01:59:10 +0500

    fixed dev url

--- src/lib/config/website.ts
+++ src/lib/config/website.ts
@@ -4,9 +4,9 @@ export const websiteConfig: websiteConfigType = {
 	author: 'skyquest',
 	title: 'svelte OFA',
 	description: 'Sveltekit one for all!',
-	url: dev ? 'http://localhost:5174' : 'https://svelteofa--realskyquest.pages.dev',
+	url: dev ? 'http://localhost:5173' : 'https://svelteofa--realskyquest.pages.dev',
 	contactEmail: 'example@example.com',
-	githubPage: 'https://github.com/realskyquest/svelteOFA',
+	githubPage: 'https://github.com/realskyquest/svelteOFA/',
 	socialMedia: undefined
 };
 
