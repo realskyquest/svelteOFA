65afb75107710856c6ac64609419e0a3b6915e0f
realskyquest <135049479+realskyquest@users.noreply.github.com>
2024-01-22 02:04:16 +0500

    Added schema markup component

---
+++ src/lib/seo/SchemaOrg.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let schemaOrgObject: object;
+	const jsonLdString: string = JSON.stringify(schemaOrgObject);
+	const jsonLdScript: string = `
+ <script type="application/ld+json">
+ ${jsonLdString}
+ ${'<'}/script>
+ `;
+</script>
+
+<svelte:head>
+	{@html jsonLdScript}
+</svelte:head>
