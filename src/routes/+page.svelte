2a50c006bb8c8e6e5b8e03025cab107777ea8b63
realskyquest <135049479+realskyquest@users.noreply.github.com>
2024-01-22 02:01:59 +0500

    Updated links and added schema markup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,37 @@
 <script lang="ts">
-	import { websiteConfig } from '$lib/config/website';
+	import { websiteConfig } from '$config/website';
 	import { page } from '$app/stores';
-	import Website from '$lib/seo/opengraph/Website.svelte';
-	import SummaryLargeImage from '$lib/seo/twitter/SummaryLargeImage.svelte';
+	import type { PageData } from './$types';
 
-	const pageTitle = 'Home';
-	const pageDescription = 'My homepage.';
-	const pageKeywords = 'home, main page';
+	import Website from '$seo/opengraph/Website.svelte';
+	import SummaryLargeImage from '$seo/twitter/SummaryLargeImage.svelte';
+	import SchemaOrg from '$seo/SchemaOrg.svelte';
+
+	export let data: PageData;
 </script>
 
 <svelte:head>
-	<title>{pageTitle}</title>
+	<title>{data.pageTitle}</title>
 	<link rel="canonical" href={$page.url.href} />
-	<meta name="description" content={pageDescription} />
+	<meta name="description" content={data.pageDescription} />
 	<meta name="robots" content="all" />
-	<meta name="keywords" content={pageKeywords} />
 </svelte:head>
 
 <SummaryLargeImage
-	title={pageTitle}
-	description={pageDescription}
-	image="/pwa-512x512.png"
+	title={data.pageTitle}
+	description={data.pageDescription}
+	image="{websiteConfig.url}/pwa-512x512.png"
 	imageAlt="page banner"
 />
 <Website
-	title={pageTitle}
-	description={pageDescription}
+	title={data.pageTitle}
+	description={data.pageDescription}
 	image="/pwa-512x512.png"
 	imageWidth="512"
 	imageHeight="512"
 	imageAlt="page banner"
 />
+<SchemaOrg schemaOrgObject={data.pageSchema} />
 
 <section class="mt-10 mb-20">
 	<article class="prose">
@@ -39,3 +40,11 @@
 		<p>PWA support added!</p>
 	</article>
 </section>
+
+<!-- <IImage
+	alt="coffee cup"
+	src="https://realskyquest.imgix.net/images/coffee.jpg"
+	width={600}
+	height={450}
+	layout="constrained"
+/> -->
