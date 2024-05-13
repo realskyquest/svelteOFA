3007c35097855a21ebacc70108c3e57399a483a3
realskyquest <135049479+realskyquesy@users.noreply.github.com>
2024-05-14 00:26:13 +0500

    Updated gitignore for drawio, added git attributes, minor changes, updated design.drawio

--- src/lib/collections/config.ts
+++ src/lib/collections/config.ts
@@ -3,7 +3,7 @@ import { dev } from '$app/environment';
 export const appConfig: APP_config = {
 	author: 'realSkyQuest',
 	title: 'Svelte OFA',
-	description: 'Svelte OFA: One for All!, A simple template to start your SvelteKit project.',
+	description: 'Svelte OFA: One for All!, A standard template for your SvelteKit app.',
 	url: dev == true ? 'http://localhost:5173' : 'https://svelteofa--realskyquest.pages.dev',
 	contactEmail: 'https://github.com/realskyquest',
 	githubPage: 'https://github.com/realskyquest/svelteofa',
